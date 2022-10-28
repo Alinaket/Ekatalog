4c0aa0916f32d6d7be1d7478e60c49534b783477
Alinaket <take.olesja@gmail.com>
2022-10-29 02:23:01 +0300

    добававил ссылку

--- resources/views/product/description.blade.php
+++ resources/views/product/description.blade.php
@@ -296,6 +296,135 @@
             </div>
         </div>
 
+        <div class="other_headphones">
+            <h2>Купити Hator Hypergang EVO <em>Усі ціни 35 →</em></h2>
+            <div class="cardList">
+                <div class="card active">
+                    <div class="img_product">
+                        <img src="https://photo.kvshop.com.ua/products/113/113871/hator-hypergang-evo-hta-810.b.jpg" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>Hator Hypergang EVO <span>(HTA-810)</span></h3>
+                        <p>тип пристрою - гарнітура, Тип - геймерські (ігрові), підключення - дротове, конструкція - повнорозмірні, тип кріплення - дуга...
+                            щеKvshop.com.uaМенше рокуДоставка: по ХустуГарантія: від виробникаПоскаржитись</p>
+                    </div>
+                    <div class="color">
+                        <div class="img_color">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Black_colour.jpg/640px-Black_colour.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="price">
+                        <span>1 999 грн.</span>
+                        <p>Перейти в магазин</p>
+                    </div>
+                    <div class="logo_shop">
+                        <div class="img_logo">
+                            <img src="https://ek.ua/logo_shop/115179.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img_product">
+                        <img src="https://photo.kvshop.com.ua/products/113/113871/hator-hypergang-evo-hta-810.b.jpg" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>Гарнітура HATOR Hypergang EVO<span>(HTA-810)</span></h3>
+                        <p>FOXTROT.UAЗ нами 6 роківДоставка: по Хусту</p>
+                    </div>
+                    <div class="color">
+                        <div class="img_color">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Black_colour.jpg/640px-Black_colour.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="price">
+                        <span>2 098 грн.</span>
+                        <p>Перейти в магазин</p>
+                    </div>
+                    <div class="logo_shop">
+                        <div class="img_logo">
+                            <img src="https://ek.ua/logo_shop/63002.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="card active">
+                    <div class="img_product">
+                        <img src="https://retromagaz.com/uploads/products/a9/45/site_eqd7a7ads_b7650d1d.png" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>Гарнитура Hator Hypergang EVO (HTA-810) Black<span></span></h3>
+                        <p>Retromagaz.comМенше рокуДоставка: у Хуст з Києва Поскаржитись</p>
+                    </div>
+                    <div class="color">
+                        <div class="img_color">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Black_colour.jpg/640px-Black_colour.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="price">
+                        <span>1 789 грн.</span>
+                        <p>Перейти в магазин</p>
+                    </div>
+                    <div class="logo_shop">
+                        <div class="img_logo">
+                            <img src="https://ek.ua/logo_shop/90947.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img_product">
+                        <img src="https://flick.zone/wp-content/uploads/2021/05/u0438122-hator-hypergang-evo-hta-810-flick.zone_15399_nobg.png" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>Hator Hypergang EVO HTA-810<span></span></h3>
+                        <p>гарнітура, дротове, штекер 3.5 мм, 64 Ом, 110 дБ, Активне шумозаглушення - немає
+                            Flick І Все для геймерівМенше рокуДоставка: у Хуст з КиєваГарантія: 24 міс. від виробника Поскаржитись</p>
+                    </div>
+                    <div class="color">
+                        <div class="img_color">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Black_colour.jpg/640px-Black_colour.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="price">
+                        <span>1 999 грн.</span>
+                        <p>Перейти в магазин</p>
+                    </div>
+                    <div class="logo_shop">
+                        <div class="img_logo">
+                            <img src="https://ek.ua/logo_shop/112642.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="card active">
+                    <div class="img_product">
+                        <span><i class="fa-solid fa-camera"></i></span>
+                    </div>
+                    <div class="text">
+                        <h3>Hator Hypergang EVO <span>(HTA-810)</span></h3>
+                        <p>тип пристрою - гарнітура, Тип - геймерські (ігрові), підключення - дротове, конструкція - повнорозмірні, тип кріплення - дуга...
+                            щеKvshop.com.uaМенше рокуДоставка: по ХустуГарантія: від виробникаПоскаржитись</p>
+                    </div>
+                    <div class="color">
+                        <div class="img_color">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Black_colour.jpg/640px-Black_colour.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="price">
+                        <span>1 999 грн.</span>
+                        <p>Перейти в магазин</p>
+                    </div>
+                    <div class="logo_shop">
+                        <div class="img_logo">
+                            <img src="https://ek.ua/logo_shop/115179.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="more">
+                    <em></em>
+                    <p>Ще 20 пропозицій</p>
+                </div>
+            </div>
+        </div>
+
+
 <div class="copy_info">
     <div class="img">
         {{--        <span><i class="fa-solid fa-basket-shopping"></i></span>--}}
