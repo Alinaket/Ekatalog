1cd956c5eddf7472f89249bd9de407b89234ab2a
Alinaket <take.olesja@gmail.com>
2022-11-11 23:37:07 +0200

    добававил ссылку

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,29 +8,36 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function home(){
-        $products= Product::get();
+    public function home()
+    {
+        $products = Product::get();
 //        dd($products);
         return view('home')
-            ->with("products",$products);
+            ->with("products", $products);
     }
-    public  function product(Request $request){
+
+    public function product(Request $request)
+    {
         $id = $request->input("id");
-        $product= Product::where("id",$id)->first();
-        $imgs = Img::where("product_id",$id)->get();
+        $product = Product::where("id", $id)->first();
+        $imgs = Img::where("product_id", $id)->get();
         return view('product.description')
-        ->with("product", $product)
-        ->with("imgs",$imgs);
+            ->with("product", $product)
+            ->with("imgs", $imgs);
     }
-    public  function test(){
-        $product= Product::where("id",6)->first();
+
+    public function test()
+    {
+        $product = Product::where("id", 6)->first();
 //        dd($product);
         return view('test')
             ->with("product", $product);
     }
-    public  function description(){
-        $product= Product::where("id",6)->first();
-        $imgs= Img::where("product_id", 2)->get();
+
+    public function description()
+    {
+        $product = Product::where("id", 6)->first();
+        $imgs = Img::where("product_id", 2)->get();
 //        dd($imgs);
 //        dd($product);
         return view('product.description')
--- database/migrations/2022_07_30_110930_create_products_table.php
+++ database/migrations/2022_07_30_110930_create_products_table.php
@@ -20,6 +20,7 @@ class CreateProductsTable extends Migration
             $table->integer("category");
             $table->string("img");
             $table->text("description");
+            $table->text("buy");
 //            время
             $table->timestamps();
         });
