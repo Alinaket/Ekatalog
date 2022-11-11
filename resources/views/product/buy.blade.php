b81ad897efa0438dc6c47c7c37a47adb943711c5
Alinaket <take.olesja@gmail.com>
2022-11-12 00:08:51 +0200

    добававил ссылку

--- resources/views/product/buy.blade.php
+++ resources/views/product/buy.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="{{asset("css/product.css")}}">
     <link rel="stylesheet" href="{{asset("css/home.css")}}">
     <link rel="stylesheet" href="{{asset("css/recet.css")}}">
+    <link rel="stylesheet" href="{{asset("css/coment.css")}}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
@@ -166,129 +167,20 @@
                 </div>
             </div>
         </div>
-       <div class="buy other_headphones">
-           <div class="cardList">
-               <div class="card active">
-                   <div class="img_product">
-                       <img src="https://photo.kvshop.com.ua/products/113/113871/hator-hypergang-evo-hta-810.b.jpg" alt="">
-                   </div>
-                   <div class="text">
-                       <h3>Hator Hypergang EVO <span>(HTA-810)</span></h3>
-                       <p>тип пристрою - гарнітура, Тип - геймерські (ігрові), підключення - дротове, конструкція - повнорозмірні, тип кріплення - дуга...
-                           щеKvshop.com.uaМенше рокуДоставка: по ХустуГарантія: від виробникаПоскаржитись</p>
-                   </div>
-                   <div class="color">
-                       <div class="img_color">
-                           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Black_colour.jpg/640px-Black_colour.jpg" alt="">
-                       </div>
-                   </div>
-                   <div class="price">
-                       <span>1 999 грн.</span>
-                       <p>Перейти в магазин</p>
-                   </div>
-                   <div class="logo_shop">
-                       <div class="img_logo">
-                           <img src="https://ek.ua/logo_shop/115179.png" alt="">
-                       </div>
-                   </div>
-               </div>
-               <div class="card">
-                   <div class="img_product">
-                       <img src="https://photo.kvshop.com.ua/products/113/113871/hator-hypergang-evo-hta-810.b.jpg" alt="">
-                   </div>
-                   <div class="text">
-                       <h3>Гарнітура HATOR Hypergang EVO<span>(HTA-810)</span></h3>
-                       <p>FOXTROT.UAЗ нами 6 роківДоставка: по Хусту</p>
-                   </div>
-                   <div class="color">
-                       <div class="img_color">
-                           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Black_colour.jpg/640px-Black_colour.jpg" alt="">
-                       </div>
-                   </div>
-                   <div class="price">
-                       <span>2 098 грн.</span>
-                       <p>Перейти в магазин</p>
-                   </div>
-                   <div class="logo_shop">
-                       <div class="img_logo">
-                           <img src="https://ek.ua/logo_shop/63002.png" alt="">
-                       </div>
-                   </div>
-               </div>
-               <div class="card active">
-                   <div class="img_product">
-                       <img src="https://retromagaz.com/uploads/products/a9/45/site_eqd7a7ads_b7650d1d.png" alt="">
-                   </div>
-                   <div class="text">
-                       <h3>Гарнитура Hator Hypergang EVO (HTA-810) Black<span></span></h3>
-                       <p>Retromagaz.comМенше рокуДоставка: у Хуст з Києва Поскаржитись</p>
-                   </div>
-                   <div class="color">
-                       <div class="img_color">
-                           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Black_colour.jpg/640px-Black_colour.jpg" alt="">
-                       </div>
-                   </div>
-                   <div class="price">
-                       <span>1 789 грн.</span>
-                       <p>Перейти в магазин</p>
-                   </div>
-                   <div class="logo_shop">
-                       <div class="img_logo">
-                           <img src="https://ek.ua/logo_shop/90947.png" alt="">
-                       </div>
-                   </div>
-               </div>
-               <div class="card">
-                   <div class="img_product">
-                       <img src="https://flick.zone/wp-content/uploads/2021/05/u0438122-hator-hypergang-evo-hta-810-flick.zone_15399_nobg.png" alt="">
-                   </div>
-                   <div class="text">
-                       <h3>Hator Hypergang EVO HTA-810<span></span></h3>
-                       <p>гарнітура, дротове, штекер 3.5 мм, 64 Ом, 110 дБ, Активне шумозаглушення - немає
-                           Flick І Все для геймерівМенше рокуДоставка: у Хуст з КиєваГарантія: 24 міс. від виробника Поскаржитись</p>
-                   </div>
-                   <div class="color">
-                       <div class="img_color">
-                           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Black_colour.jpg/640px-Black_colour.jpg" alt="">
-                       </div>
-                   </div>
-                   <div class="price">
-                       <span>1 999 грн.</span>
-                       <p>Перейти в магазин</p>
-                   </div>
-                   <div class="logo_shop">
-                       <div class="img_logo">
-                           <img src="https://ek.ua/logo_shop/112642.png" alt="">
-                       </div>
-                   </div>
-               </div>
-               <div class="card active">
-                   <div class="img_product">
-                       <span><i class="fa-solid fa-camera"></i></span>
-                   </div>
-                   <div class="text">
-                       <h3>Hator Hypergang EVO <span>(HTA-810)</span></h3>
-                       <p>тип пристрою - гарнітура, Тип - геймерські (ігрові), підключення - дротове, конструкція - повнорозмірні, тип кріплення - дуга...
-                           щеKvshop.com.uaМенше рокуДоставка: по ХустуГарантія: від виробникаПоскаржитись</p>
-                   </div>
-                   <div class="color">
-                       <div class="img_color">
-                           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Black_colour.jpg/640px-Black_colour.jpg" alt="">
-                       </div>
-                   </div>
-                   <div class="price">
-                       <span>1 999 грн.</span>
-                       <p>Перейти в магазин</p>
-                   </div>
-                   <div class="logo_shop">
-                       <div class="img_logo">
-                           <img src="https://ek.ua/logo_shop/115179.png" alt="">
-                       </div>
-                   </div>
-               </div>
-
-           </div>
-       </div>
+        <div class="reviews">
+            <div class="text">
+                <div class="title">
+                    <h3>Відгуки про Hator Hypergang EVO <span>Додати відгук</span></h3>
+                </div>
+            </div>
+            <div class="emoji">
+                <img src="https://ek.ua/img/svg/review-smile-dark-1.svg" alt="утікла в хащу, лиш пятки засверкали :)">
+                <img src="https://ek.ua/img/svg/review-smile-dark-2.svg" alt="утікла в хащу, лиш пятки засверкали :)">
+                <img src="https://ek.ua/img/svg/review-smile-dark-3.svg" alt="утікла в хащу, лиш пятки засверкали :)">
+                <img src="https://ek.ua/img/svg/review-smile-dark-4.svg" alt="утікла в хащу, лиш пятки засверкали :)">
+            </div>
+        </div>
+    </div>
     <div class="parameters">
         <div class="selection">
             <h2>ПІДБІР ЗА ПАРАМЕТРАМИ <span><i class="fa-solid fa-plus"></i></span></h2>
