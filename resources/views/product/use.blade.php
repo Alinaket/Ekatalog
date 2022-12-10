ae7dc46c04de717aa92fbc5d5effb13deb11a75b
Alinaket <take.olesja@gmail.com>
2022-12-10 19:37:49 +0200

    доробив детьо № 2 но не точно

--- resources/views/product/use.blade.php
+++ resources/views/product/use.blade.php
@@ -5,10 +5,10 @@
             @include(".product.component.heder")
             <div class="container">
                 <div class="title">
-                    <h2>Корисні матеріали по Hator Hypergang EVOДодати посилання</h2>
+                    <h2>Корисні матеріали по Hator Hypergang EVO</h2>
                 </div>
                 <div class="cardList">
-                    <div class="card">
+                    <div class="card card_useful">
                         <div class="useful">
                             <div class="question">
                                 <p>Відгук корисний?</p>
@@ -24,13 +24,13 @@
                         <div class="text">
                             <div class="container">
                                 <h3>Обзор на Hator Hypergang HTA-810</h3>
-                                <div class="button">
-                                    <button>Додати посилання</button>
-                                </div>
                             </div>
-                            <p>https://www.overclockers.ua/peripheral/hator-hypergang/</p>
+                            <p><span>огляди</span>https://www.overclockers.ua/peripheral/hator-hypergang/</p>
                             <em class="discuss"> + Обговорити</em>
                         </div>
+                    </div>
+                    <div class="button">
+                        <button>Додати посилання</button>
                     </div>
                 </div>
             </div>
